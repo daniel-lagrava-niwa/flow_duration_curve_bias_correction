f96a3c74c64e728b239adbbdef4785717ef42767
lagravasandovald <daniel.lagravasandoval@niwa.co.nz>
2019-09-10 15:39:54 +1200

    Adding the main program for this part

--- fitting/FDC_fitter.py
+++ fitting/FDC_fitter.py
@@ -1,6 +1,18 @@
+import numpy as np
+import pandas as pd
 import xarray as xr
+import argparse
 
 import DistributionFitter
 import DistributionSelector
 import PreProcessing
 import Visualization
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_nc", help="netCDF file containing the river flow", optional=False)
+    parser.add_argument("--create-plots")
+    args = parser.parse_args()
+    return args
+
